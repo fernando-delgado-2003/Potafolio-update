2fe95ba559bc7bd6de71e567af8b9dafed51db19
Fernando Delgado Bravo <68391460+fernando-delgado-2003@users.noreply.github.com>
2021-04-02 15:30:09 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
 const d = document;
-const animationBtns = d.querySelectorAll(".animation-btn");
 
-animationBtns.forEach((btn)=>{
-    btn.addEventListener("click", (e)=>{
-        let x = e.clientX - e.target.offsetLeft;
-        let y = e.clientY - e.target.offsetTop;
-
-        let span = d.createElement("span");
-         span.style.left = `${x}px`;
-         span.style.top = `${y}px`;
-
-         btn.appendChild(span)
-
-         setTimeout(()=>{
-             span.remove()
-         }, 800);
-    })
+window.addEventListener("scroll", ()=>{
+    let header = d.querySelector("header");
+    header.classList.toggle("active", window.scrollY > 0);
+        // if(window.scrollY > 0){
+    //     header.classList.add("active")
+    // }else if(window.scrollY <= 0){
+    //     header.classList.remove("active")
+    // }
 })
+// const animationBtns = d.querySelectorAll(".animation-btn");
+
+// animationBtns.forEach((btn)=>{
+//     btn.addEventListener("click", (e)=>{
+//         let x = e.clientX - e.target.offsetLeft;
+//         let y = e.clientY - e.target.offsetTop;
+
+//         let span = d.createElement("span");
+//          span.style.left = `${x}px`;
+//          span.style.top = `${y}px`;
+
+//          btn.appendChild(span)
+
+//          setTimeout(()=>{
+//              span.remove()
+//          }, 800);
+//     })
+// })
