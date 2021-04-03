3bdd466dec128e07919e8a078926dbd099610d59
Fernando Delgado Bravo <68391460+fernando-delgado-2003@users.noreply.github.com>
2021-04-03 10:39:37 -0400

    Add files via upload
    Corrección de errores inesperados

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const d = document;
 
 window.addEventListener("scroll", ()=>{
@@ -28,34 +27,3 @@ window.addEventListener("scroll", ()=>{
 //          }, 800);
 //     })
 // })
-=======
-const d = document;
-
-window.addEventListener("scroll", ()=>{
-    let header = d.querySelector("header");
-    header.classList.toggle("active", window.scrollY > 0);
-        // if(window.scrollY > 0){
-    //     header.classList.add("active")
-    // }else if(window.scrollY <= 0){
-    //     header.classList.remove("active")
-    // }
-})
-// const animationBtns = d.querySelectorAll(".animation-btn");
-
-// animationBtns.forEach((btn)=>{
-//     btn.addEventListener("click", (e)=>{
-//         let x = e.clientX - e.target.offsetLeft;
-//         let y = e.clientY - e.target.offsetTop;
-
-//         let span = d.createElement("span");
-//          span.style.left = `${x}px`;
-//          span.style.top = `${y}px`;
-
-//          btn.appendChild(span)
-
-//          setTimeout(()=>{
-//              span.remove()
-//          }, 800);
-//     })
-// })
->>>>>>> 2fe95ba559bc7bd6de71e567af8b9dafed51db19
--- js/loader-animation.js
+++ js/loader-animation.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const containerLoader = document.querySelector("#containerLoader");
 const divsLeftAndRight = [containerLoader.querySelector(".left"), containerLoader.querySelector(".right")];
@@ -13,19 +12,3 @@ setTimeout(()=>{
     })
     containerLoader.classList.add("active")
 }, 4500)
-=======
-
-const containerLoader = document.querySelector("#containerLoader");
-const divsLeftAndRight = [containerLoader.querySelector(".left"), containerLoader.querySelector(".right")];
-setTimeout(()=>{
-    document.querySelector(".text-1").classList.add("active");
-    document.querySelector(".line").classList.add("active");
-}, 3000)
-
-setTimeout(()=>{
-    divsLeftAndRight.forEach(leftAndRight =>{
-        leftAndRight.style.display="none";
-    })
-    containerLoader.classList.add("active")
-}, 4500)
->>>>>>> 2fe95ba559bc7bd6de71e567af8b9dafed51db19
