12ba07e29559b3f448802ec98dc38e6a8256dbb5
Fernando Delgado Bravo <68391460+fernando-delgado-2003@users.noreply.github.com>
2021-08-04 00:27:18 -0400

    Add files via upload

--- js/animation-skill.js
+++ js/animation-skill.js
@@ -1,8 +1,10 @@
 const titles = document.getElementsByClassName("title");
 const descriptions = document.getElementsByClassName("description");
+const logos = document.getElementsByClassName("skill-logo");
+
 let windowHeightSkill = window.innerHeight / 1.22;
 for(let x = 0; x < titles.length; x++){
-    if(!titles[x].classList.contains("bounce-title")){
+    if(!titles[x].classList.contains("bounce-y")){
 window.addEventListener("scroll", () => {
     console.log(windowHeightSkill, "jjh")
     console.log(document.querySelectorAll(".container-skills .skill")[0].getBoundingClientRect().bottom)
@@ -10,9 +12,10 @@ window.addEventListener("scroll", () => {
     for (let i = 0; i < titles.length; i++) {
 
         if (windowHeightSkill > document.querySelectorAll(".container-skills .skill")[i].getBoundingClientRect().top) {
-            if (!titles[i].classList.contains("bounce-title")) {
-                titles[i].classList.add("bounce-title")
-                descriptions[i].classList.add("bounce-text")
+            if (!titles[i].classList.contains("bounce-y")) {
+                titles[i].classList.add("bounce-y")
+                descriptions[i].classList.add("bounce-x")
+                logos[i].classList.add("bounce-y")
             }
 
         }
